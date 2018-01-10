9b1419cc45af93fda6860f709bab60e0e8e28bbe
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2018-01-10 14:40:45 +0100

    Separate data of different stations. Simplify and clean code.

--- R_code/evalCode.R
+++ R_code/evalCode.R
@@ -1,13 +1,15 @@
 # Load tdr and solaR packages
-library(tdr)
+library(tdr) ## The following code needs version >= 0.13
 library(solaR)
 
 ## Set the working directory, using the folder where the repository has been cloned.
 ## Example: 
 ## setwd('~/GitHub/pcsol/')
 
-## Load of input and output datasets for Cabauw and Carpentras
-load('data_example/data.RData')
+## Load time series of Cabauw and Carpentras stations
+load('data_example/cabauw.RData')
+load('data_example/carpentras.RData')
+## Information about the BSRN stations
 BSRNcc <- read.csv('data_example/stations.csv')
 
 ## Load code
@@ -15,10 +17,63 @@ source('R_code/clearSky.R')
 source('R_code/csMother.R')
 
 ##################################################################
-## Load of models
+## Auxiliary functions
 ##################################################################
+## Evaluate a collection of models using meteorological data from a station
+evalModels <- function(meteo, station,
+                       G0models, D0models, Bnmodels)
+{
+  ## Estimate G0 with the collection of models
+    estimG0 <- lapply(G0models, FUN = function(model)
+    {
+        estim <- clearSky(meteo, station, model = model)
+        estim$G0
+    })
+    estimG0 <- do.call(cbind, estimG0)
+    names(estimG0) <- G0models
+    
+    ## Estimate D0 with the collection of models
+    estimD0 <- lapply(D0models, FUN = function(model)
+    {
+        estim <- clearSky(meteo, station, model = model)
+        estim$D0
+    })
+    estimD0 <- do.call(cbind, estimD0)
+    names(estimD0) <- D0models
+    
+    ## Bn
+    estimBn <- lapply(Bnmodels, FUN = function(model)
+    {
+        estim <- clearSky(meteo, station, model = model)
+        estim$Bn
+    })
+    estimBn <- do.call(cbind, estimBn)
+    names(estimBn) <- Bnmodels
 
-# define which models estimate each component
+    ## Output
+    list(G0 = estimG0,
+         D0 = estimD0,
+         Bn = estimBn)    
+}
+
+## Compute model errors using tdr::applyStats
+radStats <- function(model, vals)
+{
+  errG0 <- applyStats(model$G0, vals$G0)
+  errD0 <- applyStats(model$D0, vals$D0)
+  errBn <- applyStats(model$Bn, vals$Bn)
+  list(G0 = errG0,
+       D0 = errD0,
+       Bn = errBn)
+}
+
+##################################################################
+## Load models
+##################################################################
+## The whole list of models is:
+## print(csModels)
+
+## Define which models estimate each component
 
 myModelsG0 <- c("A2005", "ABCGS", "ASHRAE1972", "B1",  "B1979", "B2", "BCLS1979", "BD", "BD2014",
                 "BDG2007","BL1994", "BR1979","C1978","C1985","DF2014","DJW2012","DMLA1988","DPP","DPPLT","ESRA","FR1999", "H1978",
@@ -42,76 +97,64 @@ myModelsBn <- c("A2005", "AB1978", "ABCGS", "ASHRAE1972", "B1",  "B2", "BCLS1979
                 "PP1976","PS2003","PSA2000","PSI", "REST2","RSP1978","S1976","S1994","SH1977", "SP1965","sSOLIS",
                 "W1978","WL1967","YH2001","Z2006")
 
-Rad_est <- lapply(c(1:2), function(st){
-  Scc <- BSRNcc[st,]
-  Smeteo <- L[[st]]
-  
-  # G0
-  G0models <- lapply(myModelsG0, FUN = function(model){
-    estim <- clearSky(Smeteo, Scc, model = model)
-    G0estim <- estim$G0
-  })
-  G0models <- do.call(cbind, G0models)
-  names(G0models) <- myModelsG0
-  
-  # D0
-  D0models <- lapply(myModelsD0, FUN = function(model){
-    estim <- clearSky(Smeteo, Scc, model = model)
-    D0estim <- estim$D0
-  })
-  D0models <- do.call(cbind, D0models)
-  names(D0models) <- myModelsD0
-  
-  # Bn
-  Bnmodels <- lapply(myModelsBn, FUN = function(model){
-    estim <- clearSky(Smeteo, Scc, model = model)
-    Bnestim <- estim$Bn
-  })
-  Bnmodels <- do.call(cbind, Bnmodels)
-  names(Bnmodels) <- myModelsBn
-  
-  Est <- list(G0=G0models, D0=D0models, Bn=Bnmodels)    
-  Est
-})
+##################################################################
+## Evaluate models
+##################################################################
+## Cabauw
+estimCabauw <- evalModels(cabauw, BSRNcc[1,],
+                          myModelsG0, myModelsD0, myModelsBn)
+statsCabauw <- radStats(estimCabauw, cabauw)
 
-save(Rad_est, file='Rad_est.RData')
+## Carpentras
+estimCarpentras <- evalModels(carpentras, BSRNcc[2,],
+                          myModelsG0, myModelsD0, myModelsBn)
 
+statsCarpentras <- radStats(estimCarpentras, carpentras)
 
-Rad_stats <- lapply(c(1:2), function(st){
-  errModel_G0 <- applyStats(Rad_est[[st]]$G0, L[[st]]$G0)
-  errModel_D0 <- applyStats(Rad_est[[st]]$D0, L[[st]]$D0)
-  errModel_Bn <- applyStats(Rad_est[[st]]$Bn, L[[st]]$Bn)
-  Stats <- list(G0=errModel_G0, D0=errModel_D0, Bn=errModel_Bn)
-  Stats
-})
+##################################################################
+## Target Diagrams
+##################################################################
+## Example for Cabauw station
 
-save(Rad_stats, file='Rad_stats.RData')
+## Define the graphical parameters
+myTheme <- tdTheme(pch = c(21:25),
+                   col.points = 'black',
+                   fill = brewer.pal(n= 12, 'Paired'),
+                   cex = 0.75)
+## Define the legend
+myLegend <- list(columns = 2,
+                            cex = 0.8,
+                            type = 'l',
+                            space = 'right')
 
-## Example for station 1
+## G0: Display only those stations whose nRMSE is below 0.1
+idxG0 <- which(statsCabauw$G0$nrmse < 0.1)
 
-foo <- which(Rad_stats[[1]]$G0$nrmse<0.1)
+targetDiagram(statsCabauw$G0[idxG0, ],
+              groups = model,
+              type='at',
+              cuts = seq(0, .1, .025),
+              par.settings = myTheme,
+              auto.key = myLegend)
 
+## D0: Display only those stations whose nRMSE is below 0.1
+idxD0 <- which(statsCabauw$D0$nrmse < 0.1)
 
-targetDiagram(Rad_stats[[1]]$G0[foo,], groups = model,type='at',
-              cuts=seq(0, .1, .025),  par.settings=tdTheme(pch = c(21:25),
-                                                           col.points = 'black',fill = brewer.pal(n= 12, 'Paired'),cex = 0.75), 
-              auto.key=list(columns=2, cex=0.8, type='l', space='right'))
+targetDiagram(statsCabauw$D0[idxD0, ],
+              groups = model,
+              type = 'at',
+              cuts = seq(0, .1, .025),
+              par.settings = myTheme,
+              auto.key = myLegend)
 
+## Bn: Display only those stations whose nRMSE is below 0.105
+idxBn <- which(statsCabauw$Bn$nrmse < 0.105)
 
-
-foo <- which(Rad_stats[[1]]$D0$nrmse<0.1)
-
-targetDiagram(Rad_stats[[1]]$D0[foo,], groups = model,type='at',
-              cuts=seq(0, .1, .025),  par.settings=tdTheme(pch = c(21:25),
-                                                           col.points = 'black',fill = brewer.pal(n= 12, 'Paired'),cex = 0.75), 
-              auto.key=list(columns=2, cex=0.8, type='l', space='right'))
-
-
-foo <- which(Rad_stats[[1]]$Bn$nrmse<0.105)
-
-targetDiagram(Rad_stats[[1]]$Bn[foo,], groups = model,type='at',
-              cuts=seq(0, .1, .025),  par.settings=tdTheme(pch = c(21:25),
-                                                           col.points = 'black',fill = brewer.pal(n= 12, 'Paired'),cex = 0.75), 
-              auto.key=list(columns=2, cex=0.8, type='l', space='right'))
+targetDiagram(statsCabauw$Bn[idxBn,],
+              groups = model,
+              type = 'at',
+              cuts = seq(0, .1, .025),
+              par.settings = myTheme,
+              auto.key = myLegend)
 
 
